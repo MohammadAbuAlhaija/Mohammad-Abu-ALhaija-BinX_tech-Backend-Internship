6a6cc268a3c1726257a7eb66aab879a0bbaaaaf9
Mohammad Abu Alhaija <eng.mohammadabualhija@gmail>
2026-07-28 10:56:35 +0300

    Week2Day3:Write 3 async methods that each simulate a delay

---
+++ Internship/Week02/Week2Day3/Day03/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Async Operations Demo");
+
+static async Task<string> GetUsersAsync()
+{
+    await Task.Delay(2000);
+    return "Users";
+}
+
+static async Task<string> GetOrdersAsync()
+{
+    await Task.Delay(3000);
+    return "Orders";
+}
+
+static async Task<string> GetProductsAsync()
+{
+    await Task.Delay(1000);
+    return "Products";
+}
