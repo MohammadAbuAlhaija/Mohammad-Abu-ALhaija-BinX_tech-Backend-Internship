4b2319fb58c2d678bb7835ad7ba5e68f0120225d
Mohammad Abu Alhaija <eng.mohammadabualhija@gmail>
2026-09-06 12:59:57 +0300

    Complete Week 8 Day 1: Sprint 3 planning and N+1 query analysis

--- Project - CardiacPatientMonitoringSystem/Project - V3/CardiacPatientMonitoringSystem/Program.cs
+++ Project - CardiacPatientMonitoringSystem/Project - V3/CardiacPatientMonitoringSystem/Program.cs
@@ -52,9 +52,12 @@ builder.Services.AddSwaggerGen(options =>
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    )
+    options
+        .UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
 );
 
 // Application Services
